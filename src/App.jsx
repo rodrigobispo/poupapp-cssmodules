b3684c9edc5b3fa698a2fcefdb067c31715f85d3
Rodrigo Augusto Carlos Bispo <rodrigo.bispo@infrasa.gov.br>
2025-12-12 09:09:52 -0300

    feat: cria typography para boas vindas e textos gerais.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Aside } from './components/Aside'
 import { Container } from './components/Container'
 import { Main } from './components/Main'
 import { SearchInput } from './components/SearchInput'
+import { Typography } from './components/Typography'
 
 function App() {
 
@@ -10,6 +11,12 @@ function App() {
       <Aside />
       <Main>
         <SearchInput />
+        <Typography variant='h2'>
+          Olá, Rodrigo!
+        </Typography>
+        <Typography variant='body'>
+          Veja como são suas finanças hoje.
+        </Typography>
       </Main>
     </Container>
   )
